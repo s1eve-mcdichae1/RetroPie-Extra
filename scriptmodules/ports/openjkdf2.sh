85800b69af177886e15cd47405c7d97bfd5248ac
G Derber <gd.github@drbr.org>
2023-02-06 10:26:16 -0500

    Add documentation and help

--- scriptmodules/ports/openjkdf2.sh
+++ scriptmodules/ports/openjkdf2.sh
@@ -11,6 +11,9 @@
 
 rp_module_id="openjkdf2"
 rp_module_desc="OpenJKDF2 is a function-by-function reimplementation of Dark Forces 2."
+# I hate this really long line.  I couldn't get all the information to display otherwise.
+#
+rp_module_help="Data must be installed manually.\nData for Jedi Knight - Dark Forces 2 goes into $romdir/openjkdf2/openjkdf2.\nData for Jedi Knight - Mysteries of the Sith goes into $romdir/openjkdf2/openjkmots.\nFor both games the 'episode' 'player' 'resource' and 'Music' directories must be copied into their repsective folders."
 rp_module_licence="OTHER https://github.com/shinyquagsire23/OpenJKDF2/blob/master/LICENSE.md"
 rp_module_repo="git https://github.com/shinyquagsire23/OpenJKDF2.git v0.8.8"
 rp_module_section="exp"
@@ -24,6 +27,7 @@ function depends_openjkdf2() {
     getDepends "${depends[@]}"
 
     # Note: This also needs to run: "pip3 install cogapp"
+    pip3 install cogapp
 }
 
 function sources_openjkdf2() {
@@ -53,14 +57,17 @@ function game_data_openjkdf2() {
 }
 
 function add_games_openjkdf2() {
+    # If neither game data is available, create both launcher (assuming person will add data later)
+    if [[ ! -f "$romdir/openjkdf2/openjkdf2/Episode/JK1.GOB" ]] && [[ ! -f "$romdir/openjkdf2/openjkmots/Episode/JKM.GOO" ]]; then
+        addPort "$md_id" "openjkdf2" "Star Wars - Jedi Knight - Dark Forces II" "$md_inst/openjkdf2 %ROM%" ""
+        addPort "$md_id" "openjkdf2" "Star Wars - Jedi Knight - Mysteries of the Sith" "$md_inst/openjkdf2 %ROM%" "-motsCompat"
+    fi
+    # If Dark Forces 2 Data Available, create Dark Forces 2 launcher
     if [[ -f "$romdir/openjkdf2/openjkdf2/Episode/JK1.GOB" ]]; then
         addPort "$md_id" "openjkdf2" "Star Wars - Jedi Knight - Dark Forces II" "$md_inst/openjkdf2 %ROM%" ""
     fi
+    # If Mysteries of the Sith data available, create MotS launcher
     if [[ -f "$romdir/openjkdf2/openjkmots/Episode/JKM.GOO" ]]; then
-        addPort "$md_id" "openjkdf2" "Star Wars - Jedi Knight - Mysteries of the Sith" "$md_inst/openjkdf2 %ROM%" "-motsCompat"
-    fi
-    if [[ ! -f "$romdir/openjkdf2/openjkdf2/Episode/JK1.GOB" ]] && [[ ! -f "$romdir/openjkdf2/openjkmots/Episode/JKM.GOO" ]]; then
-        addPort "$md_id" "openjkdf2" "Star Wars - Jedi Knight - Dark Forces II" "$md_inst/openjkdf2 %ROM%" ""
         addPort "$md_id" "openjkdf2" "Star Wars - Jedi Knight - Mysteries of the Sith" "$md_inst/openjkdf2 %ROM%" "-motsCompat"
     fi
 }
