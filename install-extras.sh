878e21ec73bc398adda4f2d4a9b75f709d807897
Exarkuniv <48326368+Exarkuniv@users.noreply.github.com>
2024-10-29 08:33:22 -0500

    Update install-extras.sh
    removed check

--- install-extras.sh
+++ install-extras.sh
@@ -82,14 +82,6 @@ function startCmd() {
         exit
     fi
 
-    pushd $RPS_HOME
-    if ! git log --format=format:%H | grep 18c2b43000ec54d74ba016e0b8de654122377dba; then
-        echo -e "Error: Your RetroPie-Setup directory is too old.  Please update your RetroPie-Setup by the Update secton from the RetroPie menu in EmulationStation or running 'git pull origin master' inside your RetroPie-Setup directory."
-        popd
-        exit
-    fi
-    popd
-
     case "$MODE" in
         auto) runAuto ;;
         remove) removeAll ;;
