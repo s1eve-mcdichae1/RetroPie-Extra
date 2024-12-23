e3b92b1919906beb9ad958002bd8265b265481bf
s1eve-mcdichae1 <92833764+s1eve-mcdichae1@users.noreply.github.com>
2024-12-22 21:04:28 -0800

    add version check for $user -> $__user migration

--- install-extras.sh
+++ install-extras.sh
@@ -82,6 +82,12 @@ function startCmd() {
         exit
     fi
 
+    local rp_version="$(cat /opt/retropie/VERSION)"
+    if ! dpkg --compare-versions "$rp_version" ge 4.8.9 >/dev/null; then
+        echo -e "Error: RetroPie version $rp_version is not compatible with the scripts used in this repository. Please update to RetroPie version 4.8.9 or greater.\n\nAborting."
+        exit
+    fi
+
     case "$MODE" in
         auto) runAuto ;;
         remove) removeAll ;;
